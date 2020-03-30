96ac00e97bb61b61fb85ee857947153de31a352a
Pham Ba Nam <phambanam1999@gmail.com>
2020-03-30 17:50:45 +0300

    Grep
    Signed-off-by: Pham Ba Nam <phambanam1999@gmail.com>

--- src/main/java/Grep.java
+++ src/main/java/Grep.java
@@ -26,48 +26,51 @@ public class Grep {
     public void setCmdV() { this.cmdV = true; }
 
     public List<String> find() throws IOException{
-
-        File file = new File(fileName);
-        FileReader fr = new FileReader(file);
-        BufferedReader br = new BufferedReader(fr);
-
         List<String> result = new ArrayList<String>();
-        String line  = br.readLine();
-        boolean tOrF;
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(fileName));
+            String line  = br.readLine();
+            boolean tOrF;
+            StringBuilder w = new StringBuilder();
+            boolean trOf = false;
 
-        StringBuilder w = new StringBuilder();
-        boolean trOf = false;
-        //specialCharacter"!@#$%^&\\*(),.?\":{}|<>"
+            //specialCharacter"!@#$%^&\\*(),.?\":{}|<>"
+            String special ="!@#$%^&\\*(),.?\":{}|<>";
 
-        String special ="!@#$%^&\\*(),.?\":{}|<>";
-
-        for (int j = 0; j < word.length(); j++) {
-            StringBuilder t = new StringBuilder();
-            t.append(word.charAt(j));
-//            if (word.charAt(j) == '*' || word.charAt(j) == '(' || word.charAt(j) == ')'
-//                    || word.charAt(j) == '\\' || word.charAt(j) == '{' || word.charAt(j) == '}'
-//                    || word.charAt(j) == '"' || word.charAt(j) == '^') {
-            if(special.contains(t)) {
-            trOf = true;
-                w.append("\\").append(word.charAt(j));
-            } else{ w.append(word.charAt(j));
+            for (int j = 0; j < word.length(); j++) {
+                StringBuilder t = new StringBuilder();
+                t.append(word.charAt(j));
+                if(special.contains(t)) {
+                    trOf = true;
+                    w.append("\\").append(word.charAt(j));
+                } else w.append(word.charAt(j));
             }
-            System.out.println(w);
-        }
-        System.out.println(w);
 
-        while((line != null)){
-            String regex;
-            if(trOf)   regex =   (this.cmdR) ? word : (".*" + w.toString() + ".*");
-            else regex = (this.cmdR) ?  word : (".*" + word + ".*") ;
-        //https://www.geeksforgeeks.org/pattern-compilestring-method-in-java-with-examples/
+            while((line != null)){
+                String regex;
+                if(trOf)   regex =   (this.cmdR) ? word : (".*" + w.toString() + ".*");
+                else regex = (this.cmdR) ?  word : (".*" + word + ".*") ;
+                //https://www.geeksforgeeks.org/pattern-compilestring-method-in-java-with-examples/
 
-        Pattern pattern = (this.cmdI) ? (Pattern.compile(regex,Pattern.CASE_INSENSITIVE)) : (Pattern.compile(regex));
-        Matcher matcher = pattern.matcher(line);
-        tOrF = matcher.matches();
-        if(this.cmdV) tOrF = ! tOrF;
-        if(tOrF) result.add(line);
-        line = br.readLine();
+                Pattern pattern = (this.cmdI) ? (Pattern.compile(regex,Pattern.CASE_INSENSITIVE))
+                        : (Pattern.compile(regex));
+                Matcher matcher = pattern.matcher(line);
+                tOrF = matcher.matches();
+                if(this.cmdV) tOrF = ! tOrF;
+                if(tOrF) result.add(line);
+                line = br.readLine();
+            }
+            return result;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (br != null)
+                    br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         return result;
     }
